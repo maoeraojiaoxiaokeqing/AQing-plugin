3aeac96fdde9d1dedad16cce14affd9f4529054d
猫耳傲娇小刻晴 <11456091+aayhg@user.noreply.gitee.com>
2023-04-15 14:29:43 +0000

    新增
    Signed-off-by: 猫耳傲娇小刻晴 <11456091+aayhg@user.noreply.gitee.com>

---
+++ resource/help/theme/default/config.js
@@ -0,0 +1,8 @@
+export const style = {
+  "fontColor": "rgba(248, 8, 8, 1)",
+  "descColor": "rgba(232, 14, 14, 1)",
+  "contBgColor": "rgba(0, 0, 0, 0)",
+  "headerBgColor": "rgba(0, 0, 0, 0)",
+  "rowBgColor1": "rgba(0, 0, 0, 0)",
+  "rowBgColor2": "rgba(0, 0, 0, 0)"
+}
