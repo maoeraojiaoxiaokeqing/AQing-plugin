bf14a0ff888e4708839f82a0e1e78eba3c576592
明るい青紫色 <11456091+aayhg@user.noreply.gitee.com>
2023-10-04 07:18:57 +0000

    更新资源
    Signed-off-by: 明るい青紫色 <11456091+aayhg@user.noreply.gitee.com>

---
+++  resource/help/theme/default/config.js
@@ -0,0 +1,9 @@
+export const style = {
+  fontColor: '#ffffff', // 主文字颜色
+  descColor: '#e7e7e7', // 描述文字颜色
+  contBgColor: 'rgba(43, 52, 61, 0.2)', // 面板整体底色，颜色过透明可能导致阅读困难
+  contBgBlur: 2, // 面板底图毛玻璃效果，数字越大越模糊，0-10 ，可为小数
+  headerBgColor: 'rgba(34, 41, 51, 0.1)', // 板块标题栏底色
+  rowBgColor1: 'rgba(25, 28, 31, 0.1)', // 帮助奇数行底色
+  rowBgColor2: 'rgba(25, 28, 31, 0.1)' // 帮助偶数行底色
+}
