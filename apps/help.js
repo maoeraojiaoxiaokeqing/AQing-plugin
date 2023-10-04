501998bd469848de2d3e2504011a97d8ffa98e95
明るい青紫色 <11456091+aayhg@user.noreply.gitee.com>
2023-10-04 07:15:45 +0000

    更新功能
    Signed-off-by: 明るい青紫色 <11456091+aayhg@user.noreply.gitee.com>

---
+++ apps/help.js
@@ -0,0 +1,70 @@
+import lodash from 'lodash'
+import { Data } from '../components/index.js'
+import HelpTheme from './help/HelpTheme.js'
+import runtimeRender from '../components/common/runtimeRender.js'
+
+export class help extends plugin {
+	constructor() {
+		super({
+			name: '[阿晴插件]帮助',
+			dsc: '阿晴帮助',
+			event: 'message',
+			priority: 5000,
+			rule: [
+				{
+					reg: `^#阿晴帮助$`,
+					fnc: 'help'
+				},
+			]
+		})
+	}
+
+async help (e) {
+  let custom = {}
+  let help = {}
+  let { diyCfg, sysCfg } = await Data.importCfg('help')
+
+  // 兼容一下旧字段
+  if (lodash.isArray(help.helpCfg)) {
+    custom = {
+      helpList: help.helpCfg,
+      helpCfg: {}
+    }
+  } else {
+    custom = help
+  }
+
+  let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg)
+  let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList
+
+  let helpGroup = []
+
+  lodash.forEach(helpList, (group) => {
+    if (group.auth && group.auth === 'master' && !e.isMaster) {
+      return true
+    }
+
+    lodash.forEach(group.list, (help) => {
+      let icon = help.icon * 1
+      if (!icon) {
+        help.css = 'display:none'
+      } else {
+        let x = (icon - 1) % 10
+        let y = (icon - x - 1) / 10
+        help.css = `background-position:-${x * 50}px -${y * 50}px`
+      }
+    })
+
+    helpGroup.push(group)
+  })
+  let themeData = await HelpTheme.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {})
+  return await runtimeRender(e,'help/index', {
+    helpCfg: helpConfig,
+    helpGroup,
+    ...themeData,
+    element: 'default'
+  },{
+    scale: 1.6
+  })
+}
+}
